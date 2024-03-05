358093e21dce11838b4c92ba5d785385d26e643a
Mitchell-Mashburn <mitchell@re3d.org>
2024-03-05 13:57:22 -0600

    Refactor install script - cleanup

--- install.sh
+++ install.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
+
 home=/home/pi
-config=/home/pi/printer_data/config
-system=/home/pi/printer_data/config/src/system_config
+config=$home/printer_data/config
+system=$config/src/system_config
+klipper_config_repo=https://github.com/re3Dprinting/klipper_config
+branch=experimental
+packages=(raspberrypi-kernel-headers build-essential bc dkms git xinput ripgrep nmap xscreensaver*)
+services=(klipper_config lightdm_watchman)
+
 cd $home
-git clone --branch experimental https://github.com/re3Dprinting/klipper_config
-rm $config/printer.cfg
-rm $config/moonraker.conf
+git clone --branch $branch $klipper_config_repo
+
+# Remove old config files
+rm $config/printer.cfg $config/moonraker.conf
+
+# Copy new config files
 cp -a $home/klipper_config/. $config/
+
 pip3 install gitpython
-apt install raspberrypi-kernel-headers build-essential bc dkms git xinput ripgrep nmap xscreensaver* -y
+apt install ${packages[@]} -y
+
+# Copy system files
 cp $system/screensaver/.xscreensaver $home/.xscreensaver
 cp $system/klipper/klipper_config.service /etc/systemd/system/.
+cp $system/display/lightdm_watchman.service /etc/systemd/system/lightdm_watchman.service
+
+# Move system files
 mv $system/fullpageos.txt /boot/fullpageos.txt
 mv $system/config.txt /boot/config.txt
 mv $system/lightdm.conf /etc/lightdm/lightdm.conf
 mv $system/01_debian.conf /usr/share/lightdm/lightdm.conf.d/01_debian.conf
 mv $system/start_chromium_browser $home/scripts/start_chromium_browser
-cp $system/display/lightdm_watchman.service /etc/systemd/system/lightdm_watchman.service
-chmod 644 /etc/systemd/system/lightdm_watchman.service
-systemctl enable lightdm_watchman.service
-chmod 644 /etc/systemd/system/klipper_config.service
-systemctl enable klipper_config.service
+mv $home/stack-install/klipper.env $home/printer_data/systemd/klipper.env
+
+# Set permissions and enable services
+for service in ${services[@]}; do
+  chmod 644 /etc/systemd/system/$service.service
+  systemctl enable $service.service
+done
+
 sh $system/usb/setup-usbmount.sh
 sed -i "s/console=serial0,115200//g" /boot/cmdline.txt
-mv $home/stack-install/klipper.env $home/printer_data/systemd/klipper.env
 chmod +x $config/get_serial.sh
 curl -sf -L https://raw.githubusercontent.com/Sineos/useful_bits/main/Linux/fix_debian_udev.sh | sudo bash
 python3 $config/src/reload.py
-
